c6c9890d543a9590f595df34ac07248b19419ec6
Zulqarnain <thatshigh78@gmail.com>
2025-09-14 19:33:07 +0500

    Add files via upload

--- Student Attendance MS/script.js
+++ Student Attendance MS/script.js
@@ -1,45 +1,83 @@
-const dateInput = document.getElementById("attendanceDate")
 const nameInput = document.getElementById("studentName")
+const dateInput = document.getElementById("attendanceDate")
 const tableBody = document.querySelector("#attendanceTable tbody")
 
-const students = []
-rollno = 1
-window.onload() = function(){
-    const savedData = localStorage.getItem("student")
-    if(savedData){
+let students = []
+let rollNo = 1
+
+window.onload = function (){
+    const savedData = localStorage.getItem("students");
+    if (savedData) {
         students = JSON.parse(savedData)
-        rollno = students[students-1].roll+1;
+        rollNo = students[students.length -1].roll + 1 ;
     }
-    displayStudents()
-    setTodayDate()
+
+    setTodayDate();
+    displayStudents();
 }
 
+function setTodayDate(){
+    const today = new Date().toISOString().split("T")[0];
+    dateInput.value = today
+    dateInput.addEventListener("change", displayStudents)
+}
 
 function savedData(){
-    localStorage.setItem("students",JSON.stringify)
-} 
-
-function setTodayDate(){
-    const today = new Date().toISOString().split("T")[0]
-    dateInput.value = today
-    dateInput.addEventListener("change",displayStudents)
+    localStorage.setItem("students", JSON.stringify(students))
 }
 
 function addStudent(){
     const name = nameInput.value.trim();
 
-    if(name === ""){
-        alert("Enter Student Name")
+    if (name === '') {
+        alert("Student name is required")
         return
     }
- const student={
-    roll : rollno,
-    name : name,
-    attendance : {}
- }
- students.push(student)
- rollno++
- nameInput.value = ""
- savedData()
- displayStudents()
+
+    const student = {
+        roll: rollNo,
+        name: name,
+        attendance: {}
+    }
+
+    students.push(student)
+    rollNo++;
+    nameInput.value = ""
+    savedData()
+    displayStudents();
+}
+
+function displayStudents(){
+    tableBody.innerHTML = ""
+    let selectedDate = dateInput.value;
+
+    students.forEach((student) => {
+        let status = student.attendance[selectedDate] || "Not Marked";
+
+        const row = document.createElement("tr")
+
+        row.innerHTML = `
+            <td> ${student.roll}</td>
+            <td> ${student.name}</td>
+            <td> 
+                <span class="${status === 'Present' ? 'present': status === 'Absent' ? 'absent' : "" }">${status}</span><br>
+                <button class="status-btn present-btn" onclick="markAttendance(${student.roll}, 'Present')">Present</button>
+                <button class="status-btn absent-btn" onclick="markAttendance(${student.roll}, 'Absent')">Absent</button>
+            </td>
+        `;
+
+        tableBody.appendChild(row)
+    });
+}
+
+function markAttendance(roll, status){
+    const selectedDate = dateInput.value;
+    const student = students.find(s => s.roll === roll)
+
+    if (student) {
+        student.attendance[selectedDate] = status;
+    }
+
+    savedData();
+    displayStudents();
 }
