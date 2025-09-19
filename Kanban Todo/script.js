94eada2445df9c70cfbcc6199279ae3729083cf2
Zulqarnain <thatshigh78@gmail.com>
2025-09-19 22:15:26 +0500

    Add files via upload

--- Kanban Todo/script.js
+++ Kanban Todo/script.js
@@ -1,11 +1,21 @@
 const taskinput = document.querySelector("#task-input")
 const addtask = document.querySelector("#addtask")
-const list1 = document.querySelector("#list1")
 const addTask=()=>{
-    list1.innerHTML += `<div class="card" draggable="true" >${taskinput.value}</div>`
+    if (taskinput.value ==="") {return}
+//<div class="card" draggable="true" >${taskinput.value}</div>
+    const newCard = document.createElement("div")
+    newCard.classList.add ("card")
+    newCard.setAttribute ("draggable", "true")
+
+    newCard.textContent = `${taskinput.value}`
+    newCard.addEventListener("dragstart", dragStart);
+    newCard.addEventListener("dragend", dragEnd);
+  
+    list1.appendChild(newCard)
+
     taskinput.value=""
 }
-
+const list1 = document.querySelector("#list1")
 const cards = document.querySelectorAll(".card"); // draggable items
 const lists = document.querySelectorAll(".list"); // dropzones
 
@@ -27,7 +37,14 @@ function dragOver(e) {
   e.preventDefault(); // allow dropping
 }
 
+function dragEnter(e) {
+  e.preventDefault();
+  this.classList.add("over"); // visual feedback
+}
 
+function dragLeave(e) {
+  this.classList.remove("over");
+}
 
 function dragDrop(e) {
   this.classList.remove("over");
@@ -44,5 +61,7 @@ for (const card of cards) {
 
 for (const list of lists) {
   list.addEventListener("dragover", dragOver);
+  list.addEventListener("dragenter", dragEnter);
+  list.addEventListener("dragleave", dragLeave);
   list.addEventListener("drop", dragDrop);
 }
